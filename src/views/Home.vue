685e1d90443fde32e19c320f82bc8ee0f00d2b15
vinandros <vinandros@gmail.com>
2021-10-01 23:20:51 -0600

    L3 | File components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="events">
+    <h4>Events for Good</h4>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import EventCard from "@/components/EventCard.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    EventCard,
   },
+  data(){
+    return {
+      events:[{
+        id: 5928101,
+        category: 'animal welfare',
+        title: 'Cat Adoption Day',
+        description: 'Find your new feline friend at this event.',
+        location: 'Meow Town',
+        date: 'January 28, 2022',
+        time: '12:00',
+        petsAllowed: true,
+        organizer: 'Kat Laydee'
+      }],
+      
+    }
+  }
 };
 </script>
+
+<style scoped>
+.events{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
