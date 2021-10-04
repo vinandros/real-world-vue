6223c634235065999166a81a5dd0092caf9fbcea
vinandros <vinandros@gmail.com>
2021-10-04 11:13:39 -0600

    API calls | Axios | Services | components lifecycle hooks

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -8,6 +8,7 @@
 <script>
 // @ is an alias to /src
 import EventCard from "@/components/EventCard.vue";
+import EventService from "../services/eventService"
 
 export default {
   name: "Home",
@@ -16,19 +17,17 @@ export default {
   },
   data(){
     return {
-      events:[{
-        id: 5928101,
-        category: 'animal welfare',
-        title: 'Cat Adoption Day',
-        description: 'Find your new feline friend at this event.',
-        location: 'Meow Town',
-        date: 'January 28, 2022',
-        time: '12:00',
-        petsAllowed: true,
-        organizer: 'Kat Laydee'
-      }],
-      
+      events:[],
     }
+  },
+  created(){
+    EventService.getEvents().then(
+      (res) => {
+        this.events = res.data
+      }
+    ).catch((error) => {
+      console.log(error)
+    })
   }
 };
 </script>
