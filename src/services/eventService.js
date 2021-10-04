5740611e3967b2b0ac00eb7bb0d75ead6cddd9cb
vinandros <vinandros@gmail.com>
2021-10-04 17:56:22 -0600

    Pagination

--- src/services/eventService.js
+++ src/services/eventService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/Code-Pop/Real-World_Vue-3',
+  baseURL: 'https://my-json-server.typicode.com/Code-Pop/Touring-Vue-Router',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
@@ -10,8 +10,8 @@ const apiClient = axios.create({
 })
 
 export default {
-  getEvents() {
-    return apiClient.get('/events')
+  getEvents(per_page, page) {
+    return apiClient.get(`/events?_limit=${per_page}&_page=${page}`)
   },
   getEvent(eventID) {
     return apiClient.get(`/events/${eventID}`)
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,6 +2,31 @@
   <div class="events">
     <h1>Events for Good</h1>
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <div class="pagination">
+      <router-link
+        id="page-prev"
+        rel="prev"
+        v-if="page != 1"
+        :to="{ name: 'EventList', query: { page: page - 1 } }"
+        >&#60; Previous</router-link
+      >
+      <router-link 
+        v-for="( page, index ) in pages"
+        :key="index + 1 "
+        :to="{ name: 'EventList', query: { page: index + 1 } }"
+        class="pages-link"
+        active-class="active-page"
+        >{{index + 1}}</router-link
+      >
+      
+      <router-link
+        id="page-next"
+        rel="next"
+        v-if="hasNextPage"
+        :to="{ name: 'EventList', query: { page: page + 1 } }"
+        >Next &#62;</router-link
+      >
+    </div>
   </div>
 </template>
 
@@ -9,25 +34,49 @@
 // @ is an alias to /src
 import EventCard from '@/components/EventCard.vue'
 import EventService from '../services/eventService'
-
+import { watchEffect } from 'vue'
 export default {
   name: 'Home',
+  props: ['page'],
   components: {
     EventCard,
   },
   data() {
     return {
       events: [],
+      totalEvents: 0,
+      totalPages: 0,
+      currentPage: 0
     }
   },
   created() {
-    EventService.getEvents()
-      .then((res) => {
-        this.events = res.data
-      })
-      .catch((error) => {
-        console.log(error)
-      })
+    watchEffect(() => {
+      EventService.getEvents(2, this.page)
+        .then((res) => {
+          this.events = res.data
+          this.totalEvents = res.headers['x-total-count']
+          this.totalPages = Math.ceil(this.totalEvents / 2)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    })
+  },
+  computed: {
+    hasNextPage() {
+      return this.page < this.totalPages
+    },
+    pages(){
+      console.log( [...Array(this.totalPages).keys()])
+      return [...Array(this.totalPages).keys()]
+    },
+    active(index){
+      if(this.page === index){
+        return{ textDecoration: "underline"}
+      }
+      return ""
+    }
+    
   },
 }
 </script>
@@ -37,5 +86,34 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+.pagination {
+  display: flex;
+  width: 290px;
+}
+
+.active-page{
+  text-decoration: underline;
+}
+
+.pages-link{
+  color: #2c3e50;
+  margin:0 10px;
+  text-decoration: none;
+}
+
+#page-prev {
+  text-align: left;
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+ 
+}
+
+#page-next {
+  text-align: right;
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
 }
 </style>
